5d33d440eb378295385583299056e9c685c1c0a8
Diego Rodriguez <36611253+asciidiego@users.noreply.github.com>
2025-05-09 03:16:16 -0700

    init v2

--- index.ts
+++ index.ts
@@ -1,9 +1,38 @@
-Bun.serve({
-    routes: {
-        "/": new Response("Hello World"),
-    },
+/* Dependencies */
+import { serve } from "bun";
 
-    fetch(request, server) {
-        return new Response("Not Found", { status: 404 });
+/* Pages */
+import homepage from "./index.html"
+
+const log = console.log.bind(console);
+
+const server = serve({
+    routes: {
+      // ** HTML imports **
+      // Bundle & route index.html to "/". This uses HTMLRewriter to scan the HTML for `<script>` and `<link>` tags, run's Bun's JavaScript & CSS bundler on them, transpiles any TypeScript, JSX, and TSX, downlevels CSS with Bun's CSS parser and serves the result.
+      "/": homepage,
+      "/writing/:id": async req => {
+
+        const { id } = req.params;
+            const writing = await Bun.file(`./writing/${id}`).text();
+            return new Response(writing, {
+                headers: {
+                    "Content-Type": "text/markdown",
+                },
+            });
+        }
     },
-});
+  
+    // Enable development mode for:
+    // - Detailed error messages
+    // - Hot reloading (Bun v1.2.3+ required)
+    development: true,
+  
+    // Prior to v1.2.3, the `fetch` option was used to handle all API requests. It is now optional.
+    // async fetch(req) {
+    //   // Return 404 for unmatched routes
+    //   return new Response("Not Found", { status: 404 });
+    // },
+  });
+  
+  console.log(`Listening on ${server.url}`);
---
+++ writing/clutter.md
@@ -0,0 +1,32 @@
+---
+TITLE: Clutter
+AUTHOR: Diego Rodriguez
+---
+
+A cluttered mind is fertile soil for wrong ideas.
+
+Our world is complex but our minds can help us navigate it.  But
+unlike other vehicles or tools of modern age, we can't easily replace
+it; we can only try to repair it.
+
+But, how does one take care of one's mind?  In the same way we take
+care of our body.
+
+Assuming our mind is in reasonable shape, we can improve our minds
+in the same way we improve reasonably fit bodies: training [^1].  Though,
+as with most worthy things in life, the steps are simple to list
+but hard to follow:
+
+- train it;
+
+- nourish it;
+
+- and let it rest and grow.
+
+
+# Footnotes
+
+[^1]: You may be wondering what can we do with extremely unhealthy
+minds.  Well, I think we can stretch the analogy further here too
+and ask the following question: what can one do with an extremely
+unhealthy body?  The answer to such hairy question is "it depends".
